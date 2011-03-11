1b439a3f208989ac4168afba658ccb79d64f2440
Joel Andres Granados <joel.granados@gmail.com>
2011-03-11 19:10:18 +0100

    Get the code to a point where it takes and keeps the picture :)

--- src/com/phenology/camtim/CameraTime.java
+++ src/com/phenology/camtim/CameraTime.java
@@ -1,22 +1,35 @@
 package com.phenology.camtim;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.VideoView;
 
-public class CameraTime extends Activity implements OnClickListener{
+public class CameraTime extends Activity
+implements OnClickListener, Camera.PictureCallback{
   	EditText minutes;
     Button startb;
     private Camera ct_camera;
-    private boolean is_ct_active;
+    private boolean is_ct_active; //is camera on
+    private boolean is_ct_running; //are we taking pictures
+    private String image_name;
     
     /** Called when the activity is first created. */
     @Override
@@ -26,6 +39,8 @@ public class CameraTime extends Activity implements OnClickListener{
         minutes = (EditText) findViewById(R.id.editText2); 
         startb = (Button) findViewById(R.id.button1);
         startb.setOnClickListener(this);
+        is_ct_active = false;
+        image_name = "nothing.jpg";
     }
 
     private int getMinutes (){
@@ -42,23 +57,90 @@ public class CameraTime extends Activity implements OnClickListener{
     	
     	VideoView vv = (VideoView) findViewById(R.id.videoView1);
     	try{
-    		ct_camera.setPreviewDisplay((SurfaceHolder) vv);
+    		ct_camera.setPreviewDisplay(vv.getHolder());
     	} catch (IOException ioe) {return false;}
     	
     	ct_camera.startPreview();
+    	is_ct_active = true;
     	return true;
     }
 
     private void stopCamera (){
     	ct_camera.stopPreview();
     	ct_camera.release();
+    	this.is_ct_active = false;
+    }
+
+    /** This is the start/stop button behavior */
+    public void onClick (View v){
+    	ct_cam_run();
     }
     
-    /** Def button behavior */
-    public void onClick (View v){
-    	if (this.is_ct_active)
-    		this.stopCamera();
-    	else
-    		this.startCamera();
+    private void ct_cam_run (){
+  		startCamera();
+  		try { //wait for the camera to adjust.
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		this.ct_camera.takePicture(null, this, this);
+		
+	  	try { //wait for the camera to adjust.
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		stopCamera();
+    }
+
+	public void onPictureTaken(byte[] data, Camera camera) {
+		Log.e("callback", "entering callback");
+		if (data != null) // Skip if no data.
+		{
+			Log.e("callback", "executing storage");
+			this.StoreByteImage(data, 100, this.image_name);
+		}
+	}
+ 
+    public boolean StoreByteImage(byte[] imageData, int quality, String expName) {
+
+    	// Using sd card. on a level 7.
+    	File root_path = Environment.getExternalStorageDirectory();
+    	File file_path = new File(root_path.getAbsolutePath()+"/Android/data/"+this.getPackageName()+"/files/"+expName);
+    	//file_path.mkdirs();
+        FileOutputStream fos = null;
+    	
+    	
+    	Log.e("store", "enter store");
+        try {
+            BitmapFactory.Options options=new BitmapFactory.Options();
+            options.inSampleSize = 5;
+
+            Log.e("store", "decoding");
+            Bitmap myImage = BitmapFactory.decodeByteArray(imageData, 0,
+                    imageData.length,options);
+
+            Log.e("store", "sending to outputstream");
+            fos = new FileOutputStream(file_path, true);
+
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            Log.e("store", "compressing");
+            myImage.compress(CompressFormat.JPEG, quality, bos);
+
+            bos.flush();
+            bos.close();
+        
+        } catch (FileNotFoundException e) {
+        	Log.e("error", "file not found");
+            Log.e("error",e.getMessage());
+        } catch (IOException e) {
+        	Log.e("error", "general exception");
+            Log.e("error",e.getMessage());
+        }
+        
+        return true;
     }
 }
